eb6ef8de4fdb282f66c89809d459d94b72e21dc6
DemonNinja91 <85861944+DemonNinja91@users.noreply.github.com>
2022-09-21 11:29:02 +0500

    Initial commit

---
+++ hulu/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants:{
+    extend:{},
+  },
+  plugins: [],
+}
